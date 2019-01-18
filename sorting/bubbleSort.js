470ac55abfb258a11b168eac60547419af86bef5
Samantha Juntilla <juntilla@hawaii.edu>
2019-01-17 21:20:59 -1000

    Creates tests and passes them.

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,6 +1,19 @@
 //CREATES A FUNCTION THAT REPLICATES BUBBLE SORT! YAY!
 const bubbleSort = (arr) => {
-
+    let copied = arr.slice(); //creates a shallow copy of the array
+    let swap; // serves as the 'switch' for the do...while loop
+    do {
+        swap = false; //sets the switch for the following code block to execute while it's in effect
+        for (let i = 0; i < copied.length - 1; ++i) { //iterates through the length of the array
+            if (copied[i] > copied[i + 1]) { // if number at any given index is greater than number at next index...
+                [copied[i], copied[i + 1]] = [copied[i + 1], copied[i]]; // switches their indices! in theory.
+                swap = true; // flip that do...while switch!
+            }
+        }
+    }
+    while (swap === true) { // sets conditional for what happens when that do...while switch is flipped
+        return copied; //returns the shallow copy of the array, ideally with everything sorted correctly
+    }
 }
 
 module.exports = bubbleSort;
--- sorting/quickSort.js
+++ sorting/quickSort.js
@@ -1,5 +1,26 @@
 // CREATES A FUNCTION THAT REPLICATES THE FUNCTIONALITY OF THE QUICK SORT ALGORITHM! YAY!
 const quickSort = (arr) => {
+    let copied = arr.slice(); //creates a shallow copy of arr
+
+    if (copied.length < 2) {
+        return copied;
+    } // if the array's length is smaller than two indices, then you don't need to sort it against itself. your job is done.
+
+    const pivot = copied[Math.floor(Math.random() * copied.length)];
+    let right = [];
+    let left = [];
+    let same = []; // accounts for if the number is the same as the pivot/maybe unnecessary?
+
+    for (let num of copied) { //for any given item in copied...
+        if (num > pivot) {
+            right.push(num); // if num is greater than the pivot, stick it to the right!
+        } else if (num < pivot) {
+            left.push(num); // if num is less than the pivot, stick it to the left!
+        } else {
+            same.push(num); // if num is the same than the pivot, stick it somewhere else!
+        }
+    }
+    return quickSort(left).concat(same).concat(quickSort(right)); // now stick 'em all together!
 
 }
 
