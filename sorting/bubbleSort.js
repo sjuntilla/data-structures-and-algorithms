90fffa7ad60c395565a6d2cc37859054622bbc30
Samantha Juntilla <juntilla@hawaii.edu>
2019-01-17 21:18:45 -1000

    Fixes master branch.

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,6 @@
+//CREATES A FUNCTION THAT REPLICATES BUBBLE SORT! YAY!
+const bubbleSort = (arr) => {
+
+}
+
+module.exports = bubbleSort;
---
+++ sorting/quickSort.js
@@ -0,0 +1,6 @@
+// CREATES A FUNCTION THAT REPLICATES THE FUNCTIONALITY OF THE QUICK SORT ALGORITHM! YAY!
+const quickSort = (arr) => {
+
+}
+
+module.exports = quickSort;
